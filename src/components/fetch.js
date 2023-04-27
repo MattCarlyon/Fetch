e74ec173403fa68ccea945e3e428f6eb00fb9e8e
Matt Carlyon <122873665+MattCarlyon@users.noreply.github.com>
2023-04-27 15:03:56 +0200

    final commit

---
+++ src/components/fetch.js
@@ -0,0 +1,45 @@
+import {useState, useEffect} from "react";
+
+const initProfile = {
+    publicRepos: null,
+    name: null,
+};
+
+const Fetch = () => {
+    //initalize our state variables
+const [profile, setProfile] = useState(initProfile);
+
+    //GET methods is for fetching some data
+    //POST method send data to an endpoint, creating something
+    //PUT method is used for updating something
+    //DELETE method to remove something
+
+// function to get data from our Github API
+const getProfile = async () => {
+    const reponse = await fetch("https://api.github.com/users/MattCarlyon");
+    const json = await reponse.json();
+
+    // update state with responce from Github API
+    setProfile({
+        publicRepos: json.public_repos, 
+        name: json.name
+    });
+
+}
+
+// Load Gihub profile from API when component mounts
+useEffect(() => {
+    getProfile();
+    // only going to load one time when the page first mounts
+
+}, [])
+
+    return <div>
+        <h1>Fetch data with useEffect</h1>
+        <h3>{`Public repositories: ${profile.publicRepos}`}</h3>
+        <h3>{`Name: ${profile.name}`}</h3>
+    
+    </div>
+};
+
+export default Fetch;
